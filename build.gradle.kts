e09476755c741c3e2c7d34d9356b6c32be4b6c45
zunza <fksfks015@naver.com>
2025-08-21 23:32:16 +0900

    ​

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
 
 group = "com.zunza"
 version = "0.0.1-SNAPSHOT"
-description = "Demo project for Spring Boot"
 
 java {
 	toolchain {
@@ -28,12 +27,17 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("io.jsonwebtoken:jjwt:0.12.6") // JWT
+	implementation("io.github.oshai:kotlin-logging-jvm:7.0.3") // Logging
 
 	runtimeOnly("com.mysql:mysql-connector-j")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testImplementation("org.springframework.security:spring-security-test")
+//	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.3.0") // kotest
+	testImplementation("io.kotest:kotest-runner-junit5-jvm:5.9.1") // kotest
+	testImplementation("com.ninja-squad:springmockk:4.0.2") // Mockk
 
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
