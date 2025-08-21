62198f2da3f001fa2dc299d5ba97398f87bf767f
zunza <fksfks015@naver.com>
2025-08-21 10:40:51 +0900

    ​

---
+++ src/main/kotlin/com/zunza/gongsamo/GongsamoApplication.kt
@@ -0,0 +1,11 @@
+package com.zunza.gongsamo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GongsamoApplication
+
+fun main(args: Array<String>) {
+	runApplication<GongsamoApplication>(*args)
+}
---
+++ src/test/kotlin/com/zunza/gongsamo/GongsamoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zunza.gongsamo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GongsamoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
