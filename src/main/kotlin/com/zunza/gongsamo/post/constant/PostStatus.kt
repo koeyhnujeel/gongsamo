d47bce5602cd9fe7b1a1219c405bcbae4e9ec65f
zunza <fksfks015@naver.com>
2025-09-01 16:14:22 +0900

    ​

--- src/main/kotlin/com/zunza/gongsamo/post/constant/PostStatus.kt
+++ src/main/kotlin/com/zunza/gongsamo/post/constant/PostStatus.kt
@@ -1,8 +1,22 @@
 package com.zunza.gongsamo.post.constant
 
-enum class PostStatus {
-    RECRUITING, // 모집중
-    RECRUITMENT_CLOSED, // 모집완료
-    TRANSACTION_COMPLETED, // 거래완료
-    CANCELLED // 취소됨
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonValue
+
+enum class PostStatus(
+    @JsonValue
+    val value: String
+) {
+    RECRUITING("모집 중"),
+    RECRUITMENT_CLOSED("모집 완료"),
+    TRANSACTION_COMPLETED("거래 완료"),
+    CANCELLED("취소");
+
+    companion object {
+        @JsonCreator
+        fun from(value: String): PostStatus {
+            return entries.find { it.value == value }
+                ?: throw IllegalArgumentException("존재하지 않는 상태 값 입니다.")
+        }
+    }
 }
